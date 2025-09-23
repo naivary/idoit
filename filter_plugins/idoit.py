cd24fb2551ddc21e5d15e1565dde134264e5c0f1
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-09-23 13:03:54 +0000

    done with this

---
+++ filter_plugins/idoit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+ 
+class FilterModule(object):
+    def filters(self):
+        return {
+            "updatepath": self.update_path
+        }
+
+    def update_path(self, matrix, current_version, desired_version):
+        current_version = int(current_version)
+        desired_version = int(desired_version)
+        path = []
+        for version, requirements in matrix.items():
+            if version == desired_version:
+                path.append(version)
+                return path
+            if version <= current_version or requirements['can_skip_update']:
+                continue
+            path.append(version)
+        return path
