1f4bad79dc5c2c656a895b8e5b061ffc298048bc
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-09-24 10:41:30 +0000

    passwords are now static and have to be changed afterwards!

--- filter_plugins/idoit.py
+++ filter_plugins/idoit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
- 
+
+
 class FilterModule(object):
     def filters(self):
-        return {
-            "updatepath": self.update_path
-        }
+        return {"updatepath": self.update_path}
 
     def update_path(self, matrix, current_version, desired_version):
         current_version = int(current_version)
@@ -14,7 +13,7 @@ class FilterModule(object):
             if version == desired_version:
                 path.append(version)
                 return path
-            if version <= current_version or requirements['can_skip_update']:
+            if version <= current_version or requirements["can_skip_update"]:
                 continue
             path.append(version)
         return path
